9431b138d3908fc0441f38cc92625f2660fb5d9e
pranavcasa <pranav@casakeyrealestate.ae>
2025-01-11 15:04:16 +0400

    Add ReusableTextWidget for category display; update CategoryWidget layout

--- lib/view/Screens/Nav screens/widget/CategoryWidget.dart
+++ lib/view/Screens/Nav screens/widget/CategoryWidget.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:online_shopping_app/Model/Category%20model.dart';
 import 'package:online_shopping_app/controller/category%20controller.dart';
+import 'package:online_shopping_app/view/Screens/Nav%20screens/widget/reusable_text_widget.dart';
 
 class Categorywidget extends StatefulWidget {
   const Categorywidget({super.key});
@@ -21,58 +22,63 @@ class _CategorywidgetState extends State<Categorywidget> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.zero, // Ensure no additional padding
-      margin: EdgeInsets.zero, // Ensure no additional margin
-      child: FutureBuilder(
-        future: futureCategories,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          } else if (snapshot.hasError) {
-            return Center(child: Text('Error: ${snapshot.error}'));
-          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-            return const Center(child: Text('No Categories'));
-          } else {
-            final categories = snapshot.data!;
-            return GridView.builder(
-              padding: EdgeInsets.zero, // Remove grid padding
-              physics: const NeverScrollableScrollPhysics(),
-              shrinkWrap: true,
-              itemCount: categories.length,
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 4,
-                crossAxisSpacing: 8,
-                mainAxisSpacing: 8,
-              ),
-              itemBuilder: (context, index) {
-                final category = categories[index];
-                return Container(
-                  child: Column(
-                    mainAxisAlignment:
-                        MainAxisAlignment.center, // Center content
-                    children: [
-                      Image.network(
-                        category.image,
-                        height: 47,
-                        width: 47,
-                      ),
-                      Text(
-                        category.name,
-                        style:
-                            GoogleFonts.quicksand(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 16
-                              ),
-                      ),
-                    ],
+    return Column(
+      children: [
+        ReusableTextWidget(title: "Categories", subtitle: "View all"),
+        Container(
+          padding: EdgeInsets.zero, // Ensure no additional padding
+          margin: EdgeInsets.zero, // Ensure no additional margin
+          child: FutureBuilder(
+            future: futureCategories,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(child: CircularProgressIndicator());
+              } else if (snapshot.hasError) {
+                return Center(child: Text('Error: ${snapshot.error}'));
+              } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                return const Center(child: Text('No Categories'));
+              } else {
+                final categories = snapshot.data!;
+                return GridView.builder(
+                  padding: EdgeInsets.zero, // Remove grid padding
+                  physics: const NeverScrollableScrollPhysics(),
+                  shrinkWrap: true,
+                  itemCount: categories.length,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 4,
+                    crossAxisSpacing: 8,
+                    mainAxisSpacing: 8,
                   ),
+                  itemBuilder: (context, index) {
+                    final category = categories[index];
+                    return Container(
+                      child: Column(
+                        mainAxisAlignment:
+                            MainAxisAlignment.center, // Center content
+                        children: [
+                          Image.network(
+                            category.image,
+                            height: 47,
+                            width: 47,
+                          ),
+                          Text(
+                            category.name,
+                            style:
+                                GoogleFonts.quicksand(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 16
+                                  ),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
                 );
-              },
-            );
-          }
-        },
-      ),
+              }
+            },
+          ),
+        ),
+      ],
     );
   }
 }
---
+++ lib/view/Screens/Nav screens/widget/reusable_text_widget.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class ReusableTextWidget extends StatelessWidget {
+  final String title;
+  final String subtitle;
+  ReusableTextWidget({super.key, required this.title, required this.subtitle});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+          title,
+            style: GoogleFonts.quicksand(
+              fontSize: 16,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+          Text(
+           subtitle,
+            style: GoogleFonts.quicksand(
+              fontSize: 16,
+              fontWeight: FontWeight.w500,
+              color: Colors.blue
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
