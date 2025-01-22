f3c875d229c7fb298d9d41f4979410355bf25929
Pranav  P S <pranavpsglobosoft@gmail.com>
2025-01-22 22:33:41 +0400

    some changes in innercategory

--- lib/utils/global variable .dart
+++ lib/utils/global variable .dart
@@ -1,7 +1,7 @@
-// String uri = "http://192.168.0.179:3000";
+String uri = "http://192.168.1.189:3000";
 
-// String uri = "http://localhost:3000";
-const String uri = "http://10.255.254.125:3000";
+//String uri = "http://localhost:3000";
+// const String uri = "http://10.255.254.125:3000";
 
 
 // the reason why the api is not workin on the android emulato 
